394d29386aa2d035d5b0eb5c5b4e62dcb2803b5a
SebaArrieta <102710333+SebaArrieta@users.noreply.github.com>
2024-08-26 22:56:14 -0400

    Update main.py

--- service2/main.py
+++ service2/main.py
@@ -1,33 +1,17 @@
 from fastapi import FastAPI, HTTPException
 import httpx
+import logging
 
 app = FastAPI()
 
+logging.basicConfig(filename='/var/log/service2/app.log', level=logging.INFO)
+logger = logging.getLogger(__name__)
+
 @app.get('/getSong/{id}')
 async def index(id: int):
-    #try:
-    response = httpx.get(f"http://service1:8000/{id}")
-    message = response.json()
-    return message
-    #except:
-        #raise HTTPException(status_code=500, detail="Server error")
-
-#docker-compose down; docker-compose up --build --remove-orphans
-
-"""
-FROM python:3.8-slim
-WORKDIR /code
-
-COPY requirements.txt /code/requirements.txt
-
-RUN apt-get update \
-    && apt-get install gcc -y \
-    && apt-get clean
-
-RUN pip install -r /code/requirements.txt \
-    && rm -rf /root/.cache/pip
-
-COPY . .
-CMD ["uvicorn", "service2.main:app", "--reload", "--host", "0.0.0.0", "--port", "8001"]
-
-"""
+    try:
+        response = httpx.get(f"http://service1:8000/{id}")
+        message = response.json()
+        return message
+    except:
+        raise HTTPException(status_code=500, detail="Server error")
