8416a6fe1feabfc29db328178aaf9dfa100b2bae
SebaArrieta <102710333+SebaArrieta@users.noreply.github.com>
2024-08-26 22:55:46 -0400

    Update main.py

--- service1/main.py
+++ service1/main.py
@@ -1,8 +1,12 @@
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 from typing import List
+import logging
 
 app = FastAPI()
+
+logging.basicConfig(filename='/var/log/service1/app.log', level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 songs_db = [
     {
@@ -26,6 +30,7 @@ class Song(BaseModel):
 @app.get('/{id}', response_model=Song)
 async def index(id: int):
     try:
+        logger.info("Service 1 - request")
         return songs_db[int(id)]
     except:
         raise HTTPException(status_code=404, detail="Song not found")
